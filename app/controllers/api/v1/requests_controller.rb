4ff892c4e1b4b60cbe476e17c7ee09d387ff14ac
Sultan <sultansailaubaev1998@gmail.com>
2018-09-30 23:04:42 +0600

    Fixed bug with movies

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -6,11 +6,13 @@ class Api::V1::RequestsController < ApplicationController
         @top50 = Anime.order(views: :desc).limit(50)
         @top50.sample(5).each do |sample|
             @shuffle[0] << sample[:id]
+            @shuffle[4] << sample[:id]
         end
 
         @shuffle_demo = [*1..Anime.count].shuffle
         @shuffle_demo -= @shuffle[0]
         @shuffle[0] += @shuffle_demo
+        @shuffle[4] += @shuffle_demo
 
         @answers_list = [*1..Anime.count].shuffle
 
@@ -20,6 +22,7 @@ class Api::V1::RequestsController < ApplicationController
                 @shuffle[1] = @shuffle[1][0..i-1]
                 @shuffle[2] = @shuffle[2][0..i-1]
                 @shuffle[3] = @shuffle[3][0..i-1]
+                @shuffle[0] = @shuffle[0][0..i-1]
 
                 break
             end
@@ -31,8 +34,6 @@ class Api::V1::RequestsController < ApplicationController
             @shuffle[2][i] = @answers[1]
             @shuffle[3][i] = @answers[2]
         end
-
-        @shuffle[4] = @shuffle[0];
 
         @shuffle[0].map! { |id| Anime.find(id)[:title_ru] }
         @shuffle[1].map! { |id| Anime.find(id)[:title_ru] }
